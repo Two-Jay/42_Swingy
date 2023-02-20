0eddc225dadc182e89d6a282ddebb8164497f44a
djeeee1272@gmail.com <djeeee1272@gmail.com>
2023-02-20 17:08:35 +0900

    Project cleaned-up. new Project init

---
+++ src/main/java/kr/ft/seoul/swingy/App.java
@@ -0,0 +1,13 @@
+package kr.ft.seoul.swingy;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
