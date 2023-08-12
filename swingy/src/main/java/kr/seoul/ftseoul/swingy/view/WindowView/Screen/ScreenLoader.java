b403d0fc3986ded30545155fe0f26ec436d890d1
Two-Jay <djeeee1272@gmail.com>
2023-08-13 06:05:22 +0900

    스크린로더에 주석 추가

--- swingy/src/main/java/kr/seoul/ftseoul/swingy/view/WindowView/Screen/ScreenLoader.java
+++ swingy/src/main/java/kr/seoul/ftseoul/swingy/view/WindowView/Screen/ScreenLoader.java
@@ -9,6 +9,18 @@ import kr.seoul.ftseoul.swingy.view.WindowView.Screen.ConcreteScreen.WelcomePhas
 
 import java.util.HashMap;
 
+/*
+ * ScreenLoader
+ * 싱글톤으로 작성했음
+ * 로딩에 필요한 콘크리트스크린 정보를 생성자에서 HashMap에 넣어둠
+ * load 메소드를 통해 콘크리트스크린을 불러옴
+ * 초기에는 단순히 스크린 로드를 위한 클래스였으나, 하위 콘크리트 스크린에서 작동하는 
+ * 이벤트 등지에서 상위 Frame에 로딩되어 있는 스크린을 바꾸는 작업을 하기 위해
+ * 해당 클래스를 싱글톤으로 작성하고 스크린 로딩에 대한 작업을 전담하도록 함
+ * 
+ * 스크린 객체 생성시 필요한 비용을 런타임이 아니라 빌드 초기에 지불하기 위해
+ * 해쉬맵에 각 인스턴스를 미리 생성하고 이를 불러오는 전략을 취함. 
+ */
 public class ScreenLoader {
     private JFrame frame;
     private Screen currentScreen = null;
